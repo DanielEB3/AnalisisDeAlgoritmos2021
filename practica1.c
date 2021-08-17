1226947a86dea2b2b6cfb3576f5369d46b132805
deb <60247011+DanielEB3@users.noreply.github.com>
2021-08-16 19:18:52 -0500

    Practica 1
    Codigo y ejecutable

---
+++ practica1.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int i, num = 0;
+	
+	printf("¿Cuantos numeros deseas ordenar? \n");
+	scanf("%d",&num);
+
+	int arreglo[num];
+	for(i=0;i<num;i++){
+		arreglo[i]= rand() % (num+1);	
+	}
+
+	int j;
+	for(i=0;i<num;i++){
+		for(j=i+1;j<num;j++){
+			if(arreglo[j]<arreglo[i]){
+				int aux=0;
+                aux = arreglo[j];
+				arreglo[j] = arreglo[i];
+				arreglo[i] = aux;	
+			}
+		}	
+	}
+	
+	printf("Numeros ordenados: \n");
+	for(i=0;i<num;i++){
+		printf("%d \n",arreglo[i]);
+	}
+}
